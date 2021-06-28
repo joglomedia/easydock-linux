2111eb82476b081290e9f8d81dec4479c03c7dd5
Edi Septriyanto <me@masedi.net>
2021-06-28 17:34:35 +0700

    Add shunit2

---
+++ shunit2/run_test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# First example from https://github.com/kward/shunit2
+
+script_under_test=$(basename "$0")
+
+# Check EasyDock executable script
+if [ -x ../src/easydock ]; then
+    ED=../src/easydock
+else
+    echo "EasyDock executable file (src/easydock) not found."
+    exit 1
+fi
+
+testEqualityEasyDockInit()
+{
+    ENV_FILE=""
+    ED_DIR=""
+
+    ${ED} init
+
+    [ -f .env.easydock ] && ENV_FILE="./.env.easydock"
+    assertEquals "./.env.easydock" "${ENV_FILE}"
+
+    [ -d .easydock ] && ED_DIR=".easydock"
+    assertEquals ".easydock" "${ED_DIR}"
+}
